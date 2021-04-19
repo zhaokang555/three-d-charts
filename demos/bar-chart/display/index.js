b27ea53a5470715980b230f59f526e7fbb49c1d9
kangzhao <forzhaokang@gmail.com>
2021-04-19 13:56:05 +0800

    refactor: exact method: initBarChart

--- demos/bar-chart/display/index.js
+++ demos/bar-chart/display/index.js
@@ -1,37 +1,3 @@
-import * as THREE from 'three';
-import {BarChartUtils, Utils} from "../../../src";
+import {initBarChart} from '../../../src';
 
-
-const keys = JSON.parse(localStorage.getItem('keys'));
-const values = JSON.parse(localStorage.getItem('values'));
-
-const scene = new THREE.Scene();
-BarChartUtils.addCubesToScene(scene, values);
-BarChartUtils.addAxesToScene(scene);
-BarChartUtils.addKeysToScene(scene, keys);
-BarChartUtils.addValuesToScene(scene, values);
-BarChartUtils.addLightToScene(scene);
-BarChartUtils.addPlaneToScene(scene);
-
-// const camera = getPerspectiveCamera(scene);
-const camera = BarChartUtils.getOrthographicCamera(scene);
-const pointer = new THREE.Vector2(-1, -1);
-document.addEventListener( 'pointermove', event => {
-    pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
-});
-const renderer = Utils.getRenderer();
-const controls = Utils.addControlsToCamera(camera, renderer, {rotate: true});
-const raycaster = new THREE.Raycaster();
-const render = () => {
-    requestAnimationFrame(render); // fallback: setTimeout 16.7
-
-    // required if controls.enableDamping or controls.autoRotate are set to true
-    controls.update();
-
-    BarChartUtils.highlightCubeInFullWindowWithPerspectiveCamera(scene, camera, raycaster, pointer);
-
-    renderer.render( scene, camera );
-};
-
-render();
+initBarChart(window);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export {default as BarChartUtils} from './bar-chart/bar-chart-utils';
+export {init as initBarChart} from './bar-chart/bar-chart';
 export {default as Utils} from './utils';
 export {default as Constant} from './constant';
 export {default as ChinaProvinceBarChartUtils} from './china-province-bar-chart/china-province-bar-chart-utils';
