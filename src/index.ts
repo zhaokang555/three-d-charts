213a8d920352f767d563fa11a53165742333e90e
kangzhao <forzhaokang@gmail.com>
2021-06-02 15:20:25 +0800

    export chinaProvinceList

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export {init as initBarChart2Args} from './bar-chart/BarChrat2Args';
 export {init as initChinaProvinceBarChart} from './chart-on-the-earch/ChinaProvinceBarChart';
 export {init as initCityRouteChart} from './chart-on-the-earch/CityRouteChart';
 export {default as tileMapTool} from './TileMapTool';
+export {chinaProvinceList} from './Constant';
