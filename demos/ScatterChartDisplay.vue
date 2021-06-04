64dff40f18a3912cbd50c8d65af7df4fce29071b
kangzhao <forzhaokang@gmail.com>
2021-06-04 11:51:48 +0800

    add mock data for ScatterChart

--- demos/ScatterChartDisplay.vue
+++ demos/ScatterChartDisplay.vue
@@ -5,19 +5,38 @@
 <script>
     import {initScatterChart} from '../src';
 
+    const randomRange = 0.4;
+    const {pow, cos, PI, random} = Math;
+
     export default {
         name: "ScatterChartDisplay",
         data() {
-          return {
-              clean: () => null,
-          }
+            return {
+                clean: () => null,
+            }
         },
         mounted() {
-            const list = JSON.parse(localStorage.getItem('list')) || [];
+            const list = this.mockData();
             this.clean = initScatterChart(list, this.$refs.container);
         },
         beforeDestroy() {
-          this.clean();
+            this.clean();
+        },
+        methods: {
+            mockData() {
+                const vertices = [];
+                for (let x = -3; x < 7; x += 0.05) {
+                    const y = 0.1 * pow(x, 3) - 0.5 * pow(x, 2) - 0.7 * x;
+
+                    const maxOffset = randomRange / 2 * 1.733;
+                    const x2 = cos(PI / 4) * (x + 3 + maxOffset); // translateX(3) rotateOnWorldAxis(new Vector3(0, 1, 0), PI / 4)
+                    vertices.push([x2 + this.random(), y + 6 + this.random(), x2 + this.random()]);
+                }
+                return vertices;
+            },
+            random() {
+                return random() * randomRange - randomRange / 2;
+            }
         },
     }
 </script>
--- src/scatter-chart/ScatterChart.ts
+++ src/scatter-chart/ScatterChart.ts
@@ -1,6 +1,6 @@
-import IPosition from '../type/IPosition';
 import { AmbientLight, BufferAttribute, BufferGeometry, Points, PointsMaterial, Scene } from 'three';
 import { addAxesToScene, addControlsToCamera, getOrthographicCamera, getRenderer } from '../CommonUtils';
+import IPosition from '../type/IPosition';
 
 export const init = (list: Array<IPosition>, container: HTMLElement) => {
     const scene = new Scene();
@@ -10,17 +10,7 @@ export const init = (list: Array<IPosition>, container: HTMLElement) => {
     const [renderer, cleanRenderer] = getRenderer(container, camera);
     const [controls, cleanControls] = addControlsToCamera(camera, renderer);
 
-    const geometry = new BufferGeometry();
-    const vertices = new Float32Array([
-        -1.0, -1.0, 1.0,
-        1.0, -1.0, 1.0,
-        1.0, 1.0, 1.0,
-        -1.0, 1.0, 1.0,
-    ]);
-    geometry.setAttribute('position', new BufferAttribute(vertices, 3));
-
-    const pointCloud = new Points(geometry, new PointsMaterial({size: 4}));
-    scene.add(pointCloud);
+    addPointCloudToScene(scene, list);
 
     let cancelId = null;
     const render = () => {
@@ -35,4 +25,16 @@ export const init = (list: Array<IPosition>, container: HTMLElement) => {
         cleanRenderer();
         cleanControls();
     };
+};
+
+const addPointCloudToScene = (scene: Scene, list: Array<IPosition>) => {
+    const geometry = new BufferGeometry();
+    const vertices = Float32Array.from(list.reduce((total, current) => {
+        total.push(...current);
+        return total;
+    }, []));
+    geometry.setAttribute('position', new BufferAttribute(vertices, 3));
+
+    const pointCloud = new Points(geometry, new PointsMaterial({size: 3}));
+    scene.add(pointCloud);
 };
