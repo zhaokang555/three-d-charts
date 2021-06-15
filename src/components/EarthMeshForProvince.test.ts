84f76f9cd8d948934acc4d75fececfdbd501a610
kangzhao <forzhaokang@gmail.com>
2021-06-15 16:39:22 +0800

    add UT: each bar has correct value

--- src/components/EarthMeshForProvince.test.ts
+++ src/components/EarthMeshForProvince.test.ts
@@ -5,6 +5,7 @@ import * as china_geo_json from '../chart-on-the-earch/china.geo.json';
 
 describe('EarthMeshForProvince.addProvinces, when add 2 provinces', () => {
     let earth;
+    let bars;
 
     beforeAll(() => {
         earth = new EarthMeshForProvince();
@@ -12,14 +13,19 @@ describe('EarthMeshForProvince.addProvinces, when add 2 provinces', () => {
             {key: '北京市', value: 1},
             {key: '上海市', value: 2},
         ]);
+        bars = earth.children.filter(c => c instanceof BarMeshWithTextOnTop);
     });
 
     test('should earth has 2 bars', () => {
-        const bars = earth.children.filter(c => c instanceof BarMeshWithTextOnTop);
         expect(bars.length).toBe(2);
     });
 
-    test('should Lines count on earth equal rings count in china.geo.json', () => {
+    test('each bar has correct value', () => {
+        expect(bars[0].value).toBeCloseTo(1);
+        expect(bars[1].value).toBeCloseTo(2);
+    });
+
+    test('should lines count on earth equal rings count in china.geo.json', () => {
         const provinces = china_geo_json.features.filter(f => f.properties.name === '北京市'
             || f.properties.name === '上海市');
 
