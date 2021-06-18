9c78ec30d27346fbd0238b2ae16e7ac348d6e32d
kangzhao <forzhaokang@gmail.com>
2021-06-18 14:39:19 +0800

    refactor EarthMeshForProvince.test.ts

--- src/components/EarthMeshForProvince.test.ts
+++ src/components/EarthMeshForProvince.test.ts
@@ -1,42 +1,31 @@
 import { EarthMeshForProvince } from './EarthMeshForProvince';
 import { BarMeshWithTextOnTop } from '../chart-on-the-earth/BarMeshWithTextOnTop';
-import { Line } from 'three';
-import * as china_geo_json from '../chart-on-the-earth/china.geo.json';
 
-describe('EarthMeshForProvince.addProvinces, when add 2 provinces', () => {
-    let earth;
-    let bars;
+describe('EarthMeshForProvince.addProvinces, when add 3 provinces', () => {
+    let earth, bars;
 
     beforeAll(() => {
         earth = new EarthMeshForProvince();
         earth.addProvinces([
-            {key: '北京市', value: 1},
-            {key: '上海市', value: 2},
+            {key: '北京市', value: 7},
+            {key: '上海市', value: 8},
+            {key: '湖北省', value: 8},
         ]);
         bars = earth.children.filter(c => c instanceof BarMeshWithTextOnTop);
     });
 
-    test('should earth has 2 bars', () => {
-        expect(bars.length).toBe(2);
+    test('should earth has 3 bars', () => {
+        expect(bars.length).toBe(3);
     });
 
-    test('each bar has correct value', () => {
-        expect(bars[0].value).toBeCloseTo(1);
-        expect(bars[1].value).toBeCloseTo(2);
+    test('should each bar has correct value', () => {
+        expect(bars[0].value).toBeCloseTo(7);
+        expect(bars[1].value).toBeCloseTo(8);
+        expect(bars[2].value).toBeCloseTo(8);
     });
 
-    test('should lines count on earth equal rings count in china.geo.json', () => {
-        const provinces = china_geo_json.features.filter(f => f.properties.name === '北京市'
-            || f.properties.name === '上海市');
-
-        const rings = [];
-        provinces.forEach(prov => {
-            prov.geometry.coordinates.forEach(polygon => {
-                polygon.forEach(ring => rings.push(ring));
-            })
-        });
-
-        const lines = earth.children.filter(c => c instanceof Line);
-        expect(lines.length).toBe(rings.length);
-    })
+    test('should each bar has correct height', () => {
+        expect(bars[0].height).toBeLessThan(bars[1].height);
+        expect(bars[1].height).toBeCloseTo(bars[2].height);
+    });
 });
